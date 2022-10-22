6645723d2caae04b57f5c64b3a34f860f25d9ca7
carlmnm <carlossmiguell3@gmail.com>
2022-10-22 13:29:51 -0300

    as cartas agora viram

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 
 
 function perguntarQuantidade(){
-    let quantasCartas;
-    while (quantasCartas === undefined ||(quantasCartas % 2) !== 0 || quantasCartas < 4 || quantasCartas > 14) {
-        quantasCartas = parseInt(prompt("Com quantas cartas você deseja jogar? \nDigite qualquer número par de 4 a 14."));
+    let quantasCartas = parseInt(prompt("Com quantas cartas você deseja jogar? Digite qualquer número par de 4 a 14."));
+    while ((quantasCartas % 2) !== 0 || quantasCartas < 4 || quantasCartas > 14) {
+        quantasCartas = parseInt(prompt("Com quantas cartas você deseja jogar? Digite qualquer número par de 4 a 14."));
     }
 
     return quantasCartas;
@@ -16,9 +16,21 @@ function adicionarCartas(){
     const elemento = document.querySelector(".disposicaoCartas")
     for (let indice = 0; indice < numeroCartas; indice ++){
         elemento.innerHTML += `
-        <li class="cartinha"> <img src="./imagens/back.png" class="papagaio"> </li>
+        <div class="cartinha" onclick="flip(this)"> 
+            <div class="frente"> 
+               frente
+            </div> 
+            <div class="verso"> 
+                <img src="./imagens/back.png" class="papagaio"> 
+            </div> 
+        </div>
         `;
     }
 }
 
 adicionarCartas()
+
+function flip(elemento){
+    elemento.classList.toggle("flipada")
+    
+}
